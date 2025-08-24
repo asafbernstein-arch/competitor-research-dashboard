77bfdd8d3d3c745557f00fe4bf515a4f39743f8b
asafbernstein-arch <asaf.bernstein@dealhub.io>
2025-08-24 12:21:37 +0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const config = {
+  googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID || '836508163319-a7iurtocebopoa261dra2g072fup9nvn.apps.googleusercontent.com',
+  googleFolderId: process.env.REACT_APP_GOOGLE_FOLDER_ID || '1fSNXEDkau3_JU4YlohqT1nCIS1SYFMXZ',
+  claudeApiKey: process.env.REACT_APP_CLAUDE_API_KEY
+};
